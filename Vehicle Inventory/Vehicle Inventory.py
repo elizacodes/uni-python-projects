2f381c8e95ca689f6837955b1d5f91675d5b13f8
Eliza <62441652+elizacodes@users.noreply.github.com>
2023-09-10 11:28:35 -0500

    Update Vehicle Inventory.py

--- Vehicle Inventory/Vehicle Inventory.py
+++ Vehicle Inventory/Vehicle Inventory.py
@@ -1,12 +1,13 @@
-# This program is designed to be used for a dealership
-# so they can have a vehicle inventory program.
+# This vehicle inventory program is designed
+# for a car dealership to keep track of their
+# vehicles for further analysis and use.
 
 class Vehicle:
-    def __init__(self,  make, model, year, mileage, color, status):
+    def __init__(self, make, model, year, mileage, color, status):
         self.make = make
         self.model = model
-        self.year = year
-        self.mileage = mileage
+        self.year = int(year)
+        self.mileage = int(mileage)
         self.color = color
         self.status = status
         
@@ -16,27 +17,30 @@ class Vehicle:
         self.year = year
         self.mileage = mileage
         self.color = color
-        self.status = status
-        
+        self.status = status 
+
     def __str__(self):
-        return("""%s %s %s
+        return("""%d %s %s
                Color: %s
-               Mileage: %s
-               Status: %s""" % (self.year,
+               Mileage: %d
+               Status: %s \n""" % (self.year,
                                 self.make,
                                 self.model,
                                 self.color,
                                 self.mileage,
                                 self.status))
 
-
-def add_vehicle(inventory):
+def enter_info():
     make = input("Enter the make of the vehicle: \n")
     model = input("Enter the vehicle model: \n")
     year = input("Enter the year of the vehicle: \n")
     mileage = input("Enter the vehicle's mileage: \n")
     color = input("Enter the color of the vehicle: \n")
     status = input("Enter the status of the vehicle (Sold/In Stock): \n")
+    return make, model, year, mileage, color, status
+
+def add_vehicle(inventory):
+    make, model, year, mileage, color, status = enter_info()
     car = Vehicle(make, model, year, mileage, color, status)
     inventory.append(car)
 
@@ -46,13 +50,8 @@ def remove_vehicle(inventory):
 
 def modify_vehicle(inventory):
     position = int(input("Enter the position of the vehicle: \n"))
-    print("Provide new details of the below. \n \n")
-    make = input("Enter the make of the vehicle: \n")
-    model = input("Enter the vehicle model: \n")
-    year = input("Enter the year of the vehicle: \n")
-    mileage = input("Enter the vehicle's mileage: \n")
-    color = input("Enter the color of the vehicle: \n")
-    status = input("Enter the status of the vehicle (Sold/In Stock): \n")
+    print("Provide new details of the below.")
+    make, model, year, mileage, color, status = enter_info()
     inventory[position].edit(make, model, year, mileage, color, status)
     
 def vehicle_output(inventory):
@@ -73,7 +72,7 @@ while user:
           1. Add New Vehicle
           2. Remove Vehicle
           3. View Inventory List
-          4. Update Inventory
+          4. Update Inventory List
           5. Export Inventory List
           6. Exit
           """)
@@ -88,7 +87,7 @@ while user:
     elif command == 4:
         modify_vehicle(inventory)
     elif command == 5:
-        print("In the works.")
+        write_txt_file(inventory)
     elif command == 6:
         user = False
     else:
